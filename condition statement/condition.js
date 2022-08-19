4344fc7cc7a5c65c401d8a7de97b2a932c44ac16
rammsanker <109334253+rammsanker54@users.noreply.github.com>
2022-08-19 18:22:43 +0530

    Add files via upload

---
+++ condition statement/condition.js
@@ -0,0 +1,128 @@
+//weather condition//
+let raining = "rain";
+let sunny = "cool";
+
+if (sunny == "cool" && raining == "cool" )
+{
+    console.log (" Don't forget to dive");
+} else 
+ {
+console.log ("sun is too hot"); }
+//marriage eligibilty//
+var w= 22;
+if (w>21)
+ 
+{
+    var gender = "male";
+    if (gender == "female")
+    {
+    console.log("fit for marriage")
+    }else{
+	console.log("unfit");}
+}
+else {
+    console.log("unfit for marriage");
+}
+
+// // switch statement//
+var numb = 5;
+switch (numb=5) {
+    case 0:
+        console.log("zero");
+        break;
+        case 1:
+            console.log("one");
+            break;
+            case 5:
+                console.log("5");
+                break;
+    default:console.log("none of the above");
+
+}
+
+
+//expression//
+//assign operator//
+var l=3;
+var m=9;
+var e;
+e=l;
+l=m;
+m=e;;
+console.log(l);
+console.log(m);
+
+// addition
+var x=92;
+var y=2;
+var z=x+y;
+console.log(z);94
+// 2 subraction
+console.log(z=x-y);90
+//  multiplication
+
+console.log(z=x*y);
+// divide
+
+console.log(z=x/y);
+
+
+//  calculate Area of Circle//
+// let dia of circle=100
+var radius=50;
+var Area= Math.PI* radius*radius;
+console.log(Area); 7853
+// calculate Area of  Triangle
+// let height= 20mm, base=10mm
+var height=40;
+var base=60;
+var Area =  (height*base)/2
+console.log(Area);
+//  Simple Interest//
+// Principal Amount=100/-
+//  Rate of Interest per year as a percent; R =  10%
+//  Time Periods in 2 yrs
+var P=100;
+var r=20;
+var t=2;
+var Interest = P*r*t/100;
+console.log(Interest);
+//compund intrest//
+//principal=500
+// time period(n)= 2years
+//rate of intrest(r)=10%
+//i=1+(r/100)*2(time)
+let q=100;
+let s=20;
+let v=2;
+let Interes = q*(1+(s)/100)**2;
+console.log(Interes);
+// //divisible of 5//
+// var num=[1,2,3,5,10,15,20,25,30,35,40,45];
+// for (var x=0; x<Numb.length;x++){
+//     if (a[x]%5==0){
+//         console.log(numb[x]);
+
+
+ 
+//odd  or even//
+
+// if
+//  (variable / 2 === 0)
+ 
+//     console. log("Even")
+//     }
+//  else {
+//     console. log("Odd")
+//     }
+// //
+
+
+//(A+B)² = A²+B² -2AB
+let A = 2;
+let B =5;
+let C= A*A+B*B-2*A*B;
+// let c= Math.sqrt(A)+Math.sqrt(B)-2*A*B;
+console.log(C);
+
+
